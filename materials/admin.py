52d82d44121e3f0d64417441b9fad898f794e22e
DeafProger <usvval@yandex.ru>
2024-09-18 12:00:58 +0700

    fix all for second lesson

--- materials/admin.py
+++ materials/admin.py
@@ -1,3 +1,7 @@
+from materials.models import Course, Lesson
 from django.contrib import admin
 
+
 # Register your models here.
+admin.site.register(Course)
+admin.site.register(Lesson)
--- materials/serializers.py
+++ materials/serializers.py
@@ -2,13 +2,25 @@ from materials.models import Course, Lesson
 from rest_framework import serializers
 
 
-class CourseSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Course
-        fields = '__all__'
-
-
 class LessonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lesson
+        fields = '__all__'
+
+
+class CourseSerializer(serializers.ModelSerializer):
+    quantity_lessons = serializers.SerializerMethodField()
+    lessons = LessonSerializer(read_only=True, many=True)
+
+    @staticmethod
+    def get_quantity_lessons(obj):
+        quantity_lessons = obj.course.all().count()
+
+        if not quantity_lessons:
+            return None
+        else:
+            return quantity_lessons
+
+    class Meta:
+        model = Course
         fields = '__all__'
