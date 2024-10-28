851c64697433f648275da88013a9bb957813c697
DeafProger <usvval@yandex.ru>
2024-10-28 13:43:52 +0700

    fix all for dockering

--- config/settings.py
+++ config/settings.py
@@ -93,7 +93,10 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('PASSWORD'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST':  os.getenv('DB_HOST'), 
+        'PORT':  os.getenv('DB_PORT'), 
+
     }
 }
 
